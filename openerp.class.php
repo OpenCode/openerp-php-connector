d7a51a03a21171ac97418faecae5ada78c055caa
Francesco Apruzzese <cescoap@gmail.com>
2013-02-08 13:10:41 +0100

    [IMP] Introduced search function

--- openerp.class.php
+++ openerp.class.php
@@ -11,9 +11,6 @@
  *
  * OpenERP XML-RPC connections methods are db, common, object , report , wizard
  *
- *
- *
- *
  */
 session_start();
 
@@ -21,143 +18,166 @@ include("xmlrpc-2.2.2/lib/xmlrpc.inc");
 
 class OpenERP {
 
-    public $server = "http://localhost:8069/xmlrpc/";
-    public $database = "test";
-    public $uid = "";/**  @uid = once user succesful login then this will asign the user id */
-    public $username = ""; /*     * * @userid = general name of user which require to login at openerp server */
-    public $passwrod = "";/** @password = password require to login at openerp server * */
+	public $server = "http://localhost:8069/xmlrpc/";
+	public $database = "test";
+	public $uid = "";/**  @uid = once user succesful login then this will asign the user id */
+	public $username = ""; /*     * * @userid = general name of user which require to login at openerp server */
+	public $passwrod = "";/** @password = password require to login at openerp server * */
 
-    public function login($username = "admin", $password="a", $database="test", $server="http://localhost:8069/xmlrpc/") {
-        $this->server = $server;
-        $this->database = $database;
-        $this->username = $username;
-        $this->passwrod = $password;
+	public function login($username = "admin", $password="a", $database="test", $server="http://localhost:8069/xmlrpc/") {
+		$this->server = $server;
+		$this->database = $database;
+		$this->username = $username;
+		$this->passwrod = $password;
 
-        $sock = new xmlrpc_client($this->server . 'common');
-        $msg = new xmlrpcmsg('login');
-        $msg->addParam(new xmlrpcval($this->database, "string"));
-        $msg->addParam(new xmlrpcval($this->username, "string"));
-        $msg->addParam(new xmlrpcval($this->passwrod, "string"));
+		$sock = new xmlrpc_client($this->server . 'common');
+		$msg = new xmlrpcmsg('login');
+		$msg->addParam(new xmlrpcval($this->database, "string"));
+		$msg->addParam(new xmlrpcval($this->username, "string"));
+		$msg->addParam(new xmlrpcval($this->passwrod, "string"));
 
-        $resp = $sock->send($msg);
-        $val = $resp->value();
-        $id = $val->scalarval();
+		$resp = $sock->send($msg);
+		$val = $resp->value();
+		$id = $val->scalarval();
 
-        if ($id > 0) {
-            $this->uid = $id;
-            return $id; //* userid of succesful login person *//
-        } else {
-            return -1; //** if userid not exists , username or password wrong.. */
-        }
-    }
+		if ($id > 0) {
+			$this->uid = $id;
+			return $id; //* userid of succesful login person *//
+			}
+		else {
+			return -1; //** if userid not exists , username or password wrong.. */
+			}
+		}
 
-    public function create($values, $model_name) {
-        $client = new xmlrpc_client("http://localhost:8069/xmlrpc/object");
-
-
-        //   ['execute','userid','password','module.name',{values....}]
-        $msg = new xmlrpcmsg('execute');
-        $msg->addParam(new xmlrpcval($this->database, "string"));  //* database name */
-        $msg->addParam(new xmlrpcval($this->uid, "int")); /* useid */
-        $msg->addParam(new xmlrpcval($this->passwrod, "string"));/** password */
-        $msg->addParam(new xmlrpcval($model_name, "string"));/** model name where operation will held * */
-        $msg->addParam(new xmlrpcval("create", "string"));/** method which u like to execute */
-        $msg->addParam(new xmlrpcval($values, "struct"));/** parameters of the methods with values....  */
-        $resp = $client->send($msg);
-
-        if ($resp->faultCode())
-            return -1; /* if the record is not created  */
-        else
-            return $resp->value()->scalarval();  /* return new generated id of record */
-    }
-
-    public function write($ids, $values, $model_name) {
-        $client = new xmlrpc_client("http://localhost:8069/xmlrpc/object");
-        //   ['execute','userid','password','module.name',{values....}]
-
-        $id_val = array();
-        $count = 0;
-        foreach ($ids as $id)
-            $id_val[$count++] = new xmlrpcval($id, "int");
+	public function create($values, $model_name) {
+		$client = new xmlrpc_client("http://localhost:8069/xmlrpc/object");
 
 
+		//   ['execute','userid','password','module.name',{values....}]
+		$msg = new xmlrpcmsg('execute');
+		$msg->addParam(new xmlrpcval($this->database, "string"));  //* database name */
+		$msg->addParam(new xmlrpcval($this->uid, "int")); /* useid */
+		$msg->addParam(new xmlrpcval($this->passwrod, "string"));/** password */
+		$msg->addParam(new xmlrpcval($model_name, "string"));/** model name where operation will held * */
+		$msg->addParam(new xmlrpcval("create", "string"));/** method which u like to execute */
+		$msg->addParam(new xmlrpcval($values, "struct"));/** parameters of the methods with values....  */
+		$resp = $client->send($msg);
 
-        $msg = new xmlrpcmsg('execute');
-        $msg->addParam(new xmlrpcval($this->database, "string"));  //* database name */
-        $msg->addParam(new xmlrpcval($this->uid, "int")); /* useid */
-        $msg->addParam(new xmlrpcval($this->passwrod, "string"));/** password */
-        $msg->addParam(new xmlrpcval($model_name, "string"));/** model name where operation will held * */
-        $msg->addParam(new xmlrpcval("write", "string"));/** method which u like to execute */
-        $msg->addParam(new xmlrpcval($id_val, "array"));/** ids of record which to be updting..   this array must be xmlrpcval array */
-        $msg->addParam(new xmlrpcval($values, "struct"));/** parameters of the methods with values....  */
-        $resp = $client->send($msg);
+		if ($resp->faultCode())
+			return -1; /* if the record is not created  */
+		else
+			return $resp->value()->scalarval();  /* return new generated id of record */
+		}
 
-        if ($resp->faultCode())
-            return -1;  /* if the record is not writable or not existing the ids or not having permissions  */
-        else
-            return $resp->value()->scalarval();  /* return new generated id of record */
-    }
+	public function write($ids, $values, $model_name) {
+		$client = new xmlrpc_client("http://localhost:8069/xmlrpc/object");
+		//   ['execute','userid','password','module.name',{values....}]
 
-    public function read($ids, $fields, $model_name) {
-        $client = new xmlrpc_client($this->server."object");
-        //   ['execute','userid','password','module.name',{values....}]
-        $client->return_type = 'phpvals';
-
-        $id_val = array();
-        $count = 0;
-        foreach ($ids as $id)
-            $id_val[$count++] = new xmlrpcval($id, "int");
-
-        $fields_val = array();
-        $count = 0;
-        foreach ($fields as $field)
-            $fields_val[$count++] = new xmlrpcval($field, "string");
+		$id_val = array();
+		$count = 0;
+		foreach ($ids as $id)
+			$id_val[$count++] = new xmlrpcval($id, "int");
 
 
 
-        $msg = new xmlrpcmsg('execute');
-        $msg->addParam(new xmlrpcval($this->database, "string"));  //* database name */
-        $msg->addParam(new xmlrpcval($this->uid, "int")); /* useid */
-        $msg->addParam(new xmlrpcval($this->passwrod, "string"));/** password */
-        $msg->addParam(new xmlrpcval($model_name, "string"));/** model name where operation will held * */
-        $msg->addParam(new xmlrpcval("read", "string"));/** method which u like to execute */
-        $msg->addParam(new xmlrpcval($id_val, "array"));/** ids of record which to be updting..   this array must be xmlrpcval array */
-        $msg->addParam(new xmlrpcval($fields_val, "array"));/** parameters of the methods with values....  */
-        $resp = $client->send($msg);
+		$msg = new xmlrpcmsg('execute');
+		$msg->addParam(new xmlrpcval($this->database, "string"));  //* database name */
+		$msg->addParam(new xmlrpcval($this->uid, "int")); /* useid */
+		$msg->addParam(new xmlrpcval($this->passwrod, "string"));/** password */
+		$msg->addParam(new xmlrpcval($model_name, "string"));/** model name where operation will held * */
+		$msg->addParam(new xmlrpcval("write", "string"));/** method which u like to execute */
+		$msg->addParam(new xmlrpcval($id_val, "array"));/** ids of record which to be updting..   this array must be xmlrpcval array */
+		$msg->addParam(new xmlrpcval($values, "struct"));/** parameters of the methods with values....  */
+		$resp = $client->send($msg);
 
-        if ($resp->faultCode())
-            return -1;  /* if the record is not writable or not existing the ids or not having permissions  */
-        else
-            return ( $resp->value() );
-    }
+		if ($resp->faultCode())
+			return -1;  /* if the record is not writable or not existing the ids or not having permissions  */
+		else
+			return $resp->value()->scalarval();  /* return new generated id of record */
+		}
 
-    public function unlink($ids , $model_name) {
-        
-        $client = new xmlrpc_client("http://localhost:8069/xmlrpc/object");
-      
-        $client->return_type = 'phpvals';
+	public function read($ids, $fields, $model_name) {
+		$client = new xmlrpc_client($this->server."object");
+		$client->return_type = 'phpvals';
 
-        $id_val = array();
-        $count = 0;
-        foreach ($ids as $id)
-            $id_val[$count++] = new xmlrpcval($id, "int");
+		$id_val = array();
+		$count = 0;
+		foreach ($ids as $id)
+			$id_val[$count++] = new xmlrpcval($id, "int");
 
-        $msg = new xmlrpcmsg('execute');
-        $msg->addParam(new xmlrpcval($this->database, "string"));  //* database name */
-        $msg->addParam(new xmlrpcval($this->uid, "int")); /* useid */
-        $msg->addParam(new xmlrpcval($this->passwrod, "string"));/** password */
-        $msg->addParam(new xmlrpcval($model_name, "string"));/** model name where operation will held * */
-        $msg->addParam(new xmlrpcval("unlink", "string"));/** method which u like to execute */
-        $msg->addParam(new xmlrpcval($id_val, "array"));/** ids of record which to be updting..   this array must be xmlrpcval array */
-//        $msg->addParam(new xmlrpcval($fields_val, "array"));/** parameters of the methods with values....  */
-        $resp = $client->send($msg);
+		$fields_val = array();
+		$count = 0;
+		foreach ($fields as $field)
+			$fields_val[$count++] = new xmlrpcval($field, "string");
 
-        if ($resp->faultCode())
-            return -1;  /* if the record is not writable or not existing the ids or not having permissions  */
-        else
-            print_r( $resp->value() );
-            //return ( $resp->value() );
-    }
+
+
+		$msg = new xmlrpcmsg('execute');
+		$msg->addParam(new xmlrpcval($this->database, "string"));  //* database name */
+		$msg->addParam(new xmlrpcval($this->uid, "int")); /* useid */
+		$msg->addParam(new xmlrpcval($this->passwrod, "string"));/** password */
+		$msg->addParam(new xmlrpcval($model_name, "string"));/** model name where operation will held * */
+		$msg->addParam(new xmlrpcval("read", "string"));/** method which u like to execute */
+		$msg->addParam(new xmlrpcval($id_val, "array"));/** ids of record which to be updting..   this array must be xmlrpcval array */
+		$msg->addParam(new xmlrpcval($fields_val, "array"));/** parameters of the methods with values....  */
+		$resp = $client->send($msg);
+
+		if ($resp->faultCode())
+			return -1;  /* if the record is not writable or not existing the ids or not having permissions  */
+		else
+			return ( $resp->value() );
+		}
+
+	public function unlink($ids , $model_name) {
+		
+		$client = new xmlrpc_client("http://localhost:8069/xmlrpc/object");
+	  
+		$client->return_type = 'phpvals';
+
+		$id_val = array();
+		$count = 0;
+		foreach ($ids as $id)
+			$id_val[$count++] = new xmlrpcval($id, "int");
+
+		$msg = new xmlrpcmsg('execute');
+		$msg->addParam(new xmlrpcval($this->database, "string"));  //* database name */
+		$msg->addParam(new xmlrpcval($this->uid, "int")); /* useid */
+		$msg->addParam(new xmlrpcval($this->passwrod, "string"));/** password */
+		$msg->addParam(new xmlrpcval($model_name, "string"));/** model name where operation will held * */
+		$msg->addParam(new xmlrpcval("unlink", "string"));/** method which u like to execute */
+		$msg->addParam(new xmlrpcval($id_val, "array"));/** ids of record which to be updting..   this array must be xmlrpcval array */
+		$resp = $client->send($msg);
+
+		if ($resp->faultCode())
+			return -1;  /* if the record is not writable or not existing the ids or not having permissions  */
+		else
+			print_r( $resp->value() );
+			return 0;
+		}
+	
+	function search($relation,$attribute,$operator,$keys) {
+		$client = new xmlrpc_client("http://localhost:8069/xmlrpc/object");
+
+		$key = array(
+				new xmlrpcval(array(new xmlrpcval($attribute , "string"),
+				new xmlrpcval($operator,"string"),
+				new xmlrpcval($keys,"string")),"array"),
+			);
+
+		$msg = new xmlrpcmsg('execute');
+		$msg->addParam(new xmlrpcval($this->database, "string"));  //* database name */
+		$msg->addParam(new xmlrpcval($this->uid, "int")); /* useid */
+		$msg->addParam(new xmlrpcval($this->passwrod, "string"));/** password */
+		$msg->addParam(new xmlrpcval($relation, "string"));
+		$msg->addParam(new xmlrpcval("search", "string"));
+		$msg->addParam(new xmlrpcval($key, "array"));
+
+		$resp = $client->send($msg);
+		$val = $resp->value();
+		$ids = $val->scalarval();
+
+		return $ids;
+		}
 
 }
 
