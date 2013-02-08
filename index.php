c93ba21e78454183d00663ef6426695b3d6edb6e
Francesco Apruzzese <cescoap@gmail.com>
2013-02-08 17:22:23 +0100

    [IMP] Example page (index.php)

--- index.php
+++ index.php
@@ -1,56 +1,28 @@
 <?php
-include_once('openerp.class.php');
 
-print "<pre/>\nOpenERP PHP connector : It support version 5 and 6.0<br/>\n Author : Tejas Tank, Tejas.tank.mca@gmail.com\n";
-print "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n\n";
+	include_once('openerp.class.php');
 
-$rpc = new OpenERP();
+	$rpc = new OpenERP();
 
-$rpc->login("admin", "admin", "6_0_11531_all", "http://6_0_11531.runbot.openerp.com:9524/xmlrpc/");
-//echo "user id : ", $x->uid;
+	$rpc->login("admin", "admin", "database_name", "database_host:8069/xmlrpc/");
 
-//echo $x->create($values,"res.partner");
-//echo "write at  " , $x->write(array(1,2,3,4,5,6) , $values , "res.partner"  );
+	// SEARCH
+	$partner_by_name_ids = $rpc->search('res.partner', 'name', 'like', 'openerp', 'string');
+	echo 'SEARCH PARTNERS BY NAME IDS:<br />';
+	print_r($partner_ids);
+	$partner_by_id_ids = $rpc->search('res.partner', 'id', '>', 100, 'int');
+	echo 'SEARCH PARTNERS BY ID IDS:<br />';
+	print_r($partner_ids);
 
+	// READ
+	$fields = array(
+		'id','name','ref'
+	);
+	$partners = $rpc->read($partner_by_name_ids, $fields, "res.partner");
 
-$fields = array(
-    'id','name','model'
-);
-
-$ids = range(0,100);
-$partners = $rpc->read($ids, $fields, "ir.model");
-
-$select = "<select name='oe_models' ONCHANGE='location = \"?model=\" +this.options[this.selectedIndex].value;'>";
-foreach ($partners as $v){
-   // print $v['name']."\n";
-   // print_r($v) . "\n";
-    $select .= "<option value='".$v['model']."' title='".$v['model']."'>".$v['name']."</option>";
-} 
-$select .= "</select>";
-print $select;
-
-
-
-
-//-------------------------------------
-
-$model =  $_GET['model'];
-
-echo $model;
-$fields = array(
-    'id','name',
-);
-
-$ids = range(0,100);
-$partners = $rpc->read($ids, $fields, $model);
-
-echo "\n\n";
-foreach ($partners as $v){
-    print $v['id']." ".$v['name']."\n";
-//    print_r($v) . "\n";
-} 
-
-
-/////print $partners = $x->unlink(array(19), "res.partner");
+	echo 'READ PARTNERS:<br />';
+	foreach ($partners as $p){
+		echo $p['ref'] . ' - ' . $p['name']. '<br />';
+	}
 
 ?>
